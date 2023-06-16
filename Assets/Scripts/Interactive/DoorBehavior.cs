cab870c7dfd164819df5e50a7230f72452c09140
LENOVALEN\zacar <zacariasv11@gmail.com>
2023-06-16 00:44:58 -0300

    Fix de triggers de las puertas que quedaban vivos cuando ya estaban abiertas

--- Assets/Scripts/Interactive/DoorBehavior.cs
+++ Assets/Scripts/Interactive/DoorBehavior.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class DoorBehavior : MonoBehaviour
 {
     public event Action PlayerTrigger;
+    private bool isOpen = false;
 
     void Start()
     {
@@ -20,7 +21,7 @@ public class DoorBehavior : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if(collision.gameObject.CompareTag("Player") && !isOpen)
         {
             PlayerTrigger?.Invoke();
         }
@@ -29,10 +30,12 @@ public class DoorBehavior : MonoBehaviour
     public void Open()
     {
         transform.GetChild(0).gameObject.SetActive(false);
+        isOpen = true;
     }
     public void Close()
     {
         transform.GetChild(0).gameObject.SetActive(true);
+        isOpen = false;
     }
 
 
