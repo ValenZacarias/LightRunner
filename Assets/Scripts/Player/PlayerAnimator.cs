c456cd76884c4eb46fe8686ef43a6c566e41f8eb
Pablo Cuello <pablo.ariel.cuello@gmail.com>
2023-07-29 01:55:51 -0300

    Animations

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -3,22 +3,32 @@ using Random = UnityEngine.Random;
 
 public class PlayerAnimator : MonoBehaviour {
     [SerializeField] private Animator _anim;    
+    [SerializeField] private GameObject _sprite;    
     [SerializeField] private ParticleSystem _trailParticles;       
 
     private PlayerController _player;
-    private SpriteRenderer _spriteRenderer;
+    private Vector3 initialSpriteScale;
+    private int scaleMultiplier;
+    // private SpriteRenderer _spriteRenderer;
+    // private SpriteRenderer _spriteDashRenderer;
 
     void Awake()
     {
         _player = GetComponentInParent<PlayerController>();
-        _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        // _spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+        // _spriteDashRenderer = _spriteRenderer.GetComponentInChildren<SpriteRenderer>();
+        initialSpriteScale = _sprite.transform.localScale;
     }
 
     void Update() {
 
         if(_player.Input.X != 0)
         {
-            _spriteRenderer.flipX = _player.Input.X < 0 ? true : false;
+            // _spriteRenderer.flipX = _player.Input.X < 0 ? true : false;
+            // _spriteDashRenderer.flipX = _player.Input.X < 0 ? true : false;
+            // _spriteRenderer.transform.localScale = new Vector3(_player.Input.X > 0 ? 1 : -1, 1, 1);
+            scaleMultiplier = _player.Input.X > 0 ? 1 : -1;
+            _sprite.transform.localScale = new Vector3(initialSpriteScale.x * scaleMultiplier, initialSpriteScale.y, initialSpriteScale.z);
         }
 
         if(_player.isDashing){
@@ -28,7 +38,7 @@ public class PlayerAnimator : MonoBehaviour {
         //if (_player == null) return;
 
         // Flip the sprite
-        //if (_player.Input.X != 0) transform.localScale = new Vector3(_player.Input.X > 0 ? 1 : -1, 1, 1); 
+        // if (_player.Input.X != 0) transform.localScale = new Vector3(_player.Input.X > 0 ? 1 : -1, 1, 1); 
 
         // Splat
         if (_player.LandingThisFrame) {
